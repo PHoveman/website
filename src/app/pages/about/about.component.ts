6908c7cf116b7dcfdac12cfcd2677838db6fcaa6
Patrick Hoveman <patchinator@hotmail.co.uk>
2024-06-19 14:56:00 +0100

    Signal changes to about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
+import { ExperienceCardComponent } from '../../components/experience-card/experience-card.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CardComponent, FooterComponent],
+  imports: [CardComponent, FooterComponent, ExperienceCardComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+  frontEndSkillsArr = signal<string[]>([
+    'HTML5',
+    'CSS & SCSS',
+    'Typescrpt (v5.1)',
+    'Angular (v17)',
+    'RxJs',
+    'Angular state management (NgXs & NgRx)',
+    'Angular libraries',
+    'testing Tools (Cypress, Jasmine, Jest)',
+    'React',
+    'Next.js'
+  ])
+  backEndSkillsArr = signal<string[]>([
+    'Node.js',
+    'Nest.js',
+    'PostgreSQL',
+    'TypeORM',
+    'AWS'
+  ])
+  otherSkillsArr = signal<string[]>([
+    'Figma',
+    'Adobe illustrator',
+    'Versioning (Git, Jira, Bitbucket'
+  ])
 }
